27bcfa92a70990bda8051b056e0b54a1876788c3
sinires <sinires@gmail.com>
2016-08-09 17:31:47 +0300

    Remove test file

---
+++ src/ObserveStorage.js
@@ -0,0 +1,164 @@
+/**
+ * Created by sinires on 08.08.16.
+ */
+"use strict";
+
+class OS {
+    /**
+     * Generation id
+     * @returns {string}
+     * @private
+     */
+    static __getId() {
+        return (`${Math.random().toString().replace("0.", "")}${Date.now()}`)
+    }
+
+    constructor() {
+        this.storage      = new Map();
+        this.listeners    = new Map();
+        this.serviceField = new Set([`on`, 'un']);
+    }
+
+    /**
+     * Generate wrappers for object
+     * @param key
+     * @param object
+     * @returns {Proxy}
+     */
+    getProxy(key, object){
+        let self = this;
+        return new Proxy(object, {
+                    get(target, field) {
+                        return target[field];
+                    },
+
+                    set(target, field, value) {
+                        if(self.serviceField.has(field))
+                            throw new Error(`Field ${field} is blocked for DB object`);
+
+                        const oldValue = target[field];
+                        target[field] = value;
+                        self.fire(key, {
+                            type:       oldValue ? "change" : "add",
+                            property:   field,
+                            oldValue:   oldValue,
+                            value:      value,
+                            object:     self.get(key)
+                        });
+                        return true
+                    },
+
+                    deleteProperty(target, field) {
+                        if (!field in target || self.serviceField.has(field)) {
+                            return false;
+                        }
+
+                        const oldValue = target[field];
+
+                        delete target[field];
+
+                        self.fire(key, {
+                            type:       "delete",
+                            property:   field,
+                            oldValue:   oldValue,
+                            value:      undefined,
+                            object:     self.get(key)
+                        });
+
+                        return true;
+                    },
+                    ownKeys(target) {
+                        let props = Object.keys(target)
+                                        .filter(function (prop) {
+                                            return !(self.serviceField.has(prop));
+                                        });
+                        return props;
+                    }
+                }
+        );
+    }
+
+    /**
+     * Adding an object to the storage
+     * @param key
+     * @param object
+     * @returns {Proxy}
+     */
+    add(...arg) {
+        let key, object;
+
+        if(arg.length == 1){
+            [object] = arg;
+             key = OS.__getId();
+        }
+        else
+            [key, object] = arg;
+
+        if (this.storage.has(key)) {
+            throw new Error(`key ${key} is already in use`);
+        }
+
+        let self = this;
+        object.on = (...arg)=> self.on(key, ...arg);
+        object.un = (...arg)=> self.un(key, ...arg);
+        
+        const proxy = this.getProxy(key, object);
+        this.listeners.set(key, new Map());
+        this.storage.set(key, proxy);
+
+        return proxy;
+    }
+
+    /**
+     * Deleting an object from the storage
+     * @param key
+     * @returns {boolean}
+     */
+    remove(key) {
+        return (this.storage.delete(key) && this.listeners.delete(key))
+    }
+
+    /**
+     * Getting an object from the repository
+     * @param key
+     * @returns {Proxy || undefined}
+     */
+    get(key) {
+        if(this.storage.has(key))
+            return this.storage.get(key);
+        else{
+            console.warn(`Element ${key} is not exist`);
+            return undefined;
+        }
+    }
+
+    /**
+     * Call object listeners
+     * @param event
+     * @param listeners
+     */
+    fireListeners(event, listeners) {
+            listeners.forEach((listener)=> {
+                setTimeout(()=> listener(event), 0);
+            })
+        }
+
+    /**
+     * Processing object events
+     * @param key
+     * @param event
+     */
+    fire(key, event) {
+        let listeners = this.listeners.get(key)
+            ,property = event.property;
+
+        listeners.has(property) && this.fireListeners(event, listeners.get(property));
+        listeners.has("*")      && this.fireListeners(event, listeners.get("*"));
+    }
+
+
+}
+
+window.ObserveStorage = new OS();
+
+
